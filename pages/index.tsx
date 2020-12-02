f3e406eeedbd1b7716a223dbb99a6db931db3063
Ngozi Ekekwe <ngozi-ekekwe>
2020-12-02 21:07:53 +0100

    chore:add typescript support

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+const Home:NextPage = () => {
+  return (
+      <div>
+        <Head>
+          <title>Ngozi Ekekwe</title>
+        </Head>
+  
+        <main>
+          <h1>Ngozi Ekekwe</h1>
+        </main>
+      </div>
+    );
+}
+
+export default Home;
